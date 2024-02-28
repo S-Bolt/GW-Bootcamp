1625c20239a6c9e8d32df9c01d18444dc6c116be
Sam <sambolton.kw@gmail.com>
2024-02-28 14:44:10 -0500

    2/27 work

---
+++ Class-Activities/03-Javascript/01-Activities/04-Stu_Hello-Variable/Unsolved/script.js
@@ -0,0 +1,17 @@
+// Write Your JavaScript Code Here
+var personName="Sam";
+var pets="5";
+var funFact ="I lied, I have no pets";
+
+console.log("My name is " +personName + ".");
+console.log("I have " + pets +" pet(s).");
+console.log("Fun fact: " + funFact + ".");
+
+personName="Daniel";
+pets="1";
+funFact="I have a dog";
+
+console.log("My name is " +personName + ".");
+console.log("I have " + pets +" pet(s).");
+console.log("Fun fact: " + funFact + ".");
+
