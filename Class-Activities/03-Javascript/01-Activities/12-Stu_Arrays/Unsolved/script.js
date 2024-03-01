5e6f7a70716afadfaea5a3ddd73f1921c71bbb06
Sam <sambolton.kw@gmail.com>
2024-03-01 15:09:07 -0500

    more tasks

---
+++ Class-Activities/03-Javascript/01-Activities/12-Stu_Arrays/Unsolved/script.js
@@ -0,0 +1,15 @@
+// WRITE YOUR CODE HERE
+let names =["joe", "bob", "angie", "mellisa"];
+    console.log(names)
+    console.log("Welcome to the class "+ names + ".");
+    
+
+
+
+
+
+
+  
+    //it's done when the first element in the array is replaced with the name of a new student.
+
+ //It's done when, after an `if` statement confirms that the first element in the array has been replaced, the message "REPLACED_NAME is in class" is logged.
---
+++ Class-Activities/03-Javascript/01-Activities/14-Stu_Iteration/Unsolved/script.js
@@ -0,0 +1,12 @@
+//WRITE YOUR CODE BELOW
+let students =["Bill", "Jill" , "Mel", "Joe" , "Jeff"];
+    console.log(students.length);
+
+for(let i=0; i < students.length; i++){
+    console.log("Great to see you," + students[i] + "!")
+}
+
+
+
+
+
