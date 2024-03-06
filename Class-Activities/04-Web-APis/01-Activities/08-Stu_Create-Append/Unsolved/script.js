476005882529f572f79af908b8b70ff8f9bb0ba2
Sam <sambolton.kw@gmail.com>
2024-03-06 16:26:43 -0500

    first few activities

---
+++ Class-Activities/04-Web-APis/01-Activities/08-Stu_Create-Append/Unsolved/script.js
@@ -0,0 +1,63 @@
+var body = document.body;
+var h1El = document.createElement("h1");
+var infoEl = document.createElement("div");
+var imgEl = document.createElement("img");
+var kittenEl = document.createElement("div");
+var nameEl = document.createElement("div");
+var favoriteEl = document.createElement("div");
+// Create ordered list element
+var listEl = document.createElement("ol");
+// Create ordered list items
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+
+h1El.textContent = "Welcome to my page";
+kittenEl.textContent = "This is my kitten 🐱.";
+nameEl.textContent = "His name is Jax.";
+favoriteEl.textContent = "My favorite foods are:";
+
+body.appendChild(h1El);
+body.appendChild(infoEl);
+infoEl.appendChild(imgEl);
+infoEl.appendChild(kittenEl);
+infoEl.appendChild(nameEl);
+body.appendChild(favoriteEl);
+// Append ordered list 
+favoriteEl.appendChild(listEl);
+
+h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+imgEl.setAttribute("src", "http://placekitten.com/200/300");
+nameEl.setAttribute("style", "font-size:25px; text-align:center;");
+kittenEl.setAttribute("style", "font-size:25px; text-align:center;");
+favoriteEl.setAttribute("style", "font-size:20px;");
+
+// TODO: Add ordered list items containing four favorite foods
+li1.textContent = `Tacos`;
+listEl.appendChild(li1);
+
+li2.textContent = `Tequilla`;
+listEl.appendChild(li2);
+
+li3.textContent = `Limes`
+listEl.appendChild(li3);
+
+li4.textContent = `The worm`;
+listEl.appendChild(li4);
+
+listEl.setAttribute(`style`, `background-color: #333333; padding: 20px;`);
+
+let liElements = document.querySelectorAll(`li`);
+
+
+liElements.forEach((li, index) =>{
+li.setAttribute(`style`, `font-color: white; padding: 5px; margin-left: 35px`);
+
+let colors = [`pink`, `yellow`, `green`, `turquoise`];
+li.style.color = colors[index % colors.length];
+
+let backgroundColors = [`turquoise`, `green`, `yellow`, `pink`];
+li.style.backgroundColor = backgroundColors[index % colors.length];
+});
