19b40a745c378854ca23457bdc75391c7943768e
Sam <sambolton.kw@gmail.com>
2024-03-22 17:19:37 -0400

    26 done

--- Class-Activities/05-Third-Party-APIs/01-Activities/26-Stu_DayJS-Format/Unsolved/assets/js/script.js
+++ Class-Activities/05-Third-Party-APIs/01-Activities/26-Stu_DayJS-Format/Unsolved/assets/js/script.js
@@ -1,16 +1,28 @@
 // Use Day.js to format the date and assign to the declared variable.
 
 // TODO: 1. What is your graduation date in the following format: Jan 1, 1999?
-
+var graduationDate = dayjs('2006-06-01').format('MMM D, YYYY');
+    $(`#1a`).text(graduationDate);
 // TODO: 2. What day of the week will 1/1/2027 be? (e.g. Is it "Monday"?)
-
+var dayOfWeek2027 = dayjs('2027-01-01').format('dddd');
+    $('#2a').text(dayOfWeek2027);
 // TODO: 3. What is the current time in the format: hours:minutes:seconds
-
+var currentTime = dayjs().format('h:mm:ss A');
+    $('#3a').text(currentTime); 
 // TODO: 4. What is the current Unix timestamp?
-
+var currentUnixTimestamp = dayjs().unix();
+    $('#4a').text(currentUnixTimestamp);
 // TODO: 5. Parse the following Unix timestamp, 1318781876, and convert into any time/date format.
-
+var unixTimestamp = 1318781876;
+var convertedTimestamp = dayjs.unix(unixTimestamp).format('YYYY-MM-DD HH:mm:ss');
+    $('#5a').text(convertedTimestamp);
 // TODO: 6. What is the difference in days between May 4, 2027 and today? Hint:
 // You can display the difference between two dayjs objects by using the dayjs
 // diff method.)
+var today = dayjs();
+var may4_2027 = dayjs('2027-05-04');
+var differenceInDays = may4_2027.diff(today, 'day');
+    $('#6a').text(differenceInDays + ` days`);
+
+
 
